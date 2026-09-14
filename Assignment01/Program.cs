f0633ac9e6cd854c91410fe67eb6aa96c7155d99
KUNTAPHAT <peetlock@gmail.com>
2026-09-14 09:30:26 +0700

    Assignment01

---
+++ Assignment01/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Assignment01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string GameTitle = "Bloodborne";
+
+            var hunterName = "Hunter";
+            var origin = "Waste of Skin";
+
+            int Level = 46;
+            int hp = 200;
+            int strength = 20;
+            int bloodEchoes = 1000;
+
+            float bloodtinge = 5.5f;    
+            double insight = 20.5;
+
+            var hunterRank = 'B';
+            bool isInNightmare = true;
+
+            Console.WriteLine("=================================");
+            Console.WriteLine($"        {GameTitle}");
+            Console.WriteLine("        HUNTER STAT ");
+            Console.WriteLine("=================================");
+            Console.WriteLine($"Name        : {hunterName}");
+            Console.WriteLine($"Origin      : {origin}");
+            Console.WriteLine($"Level       : {Level}");
+            Console.WriteLine($"HP          : {hp}");
+            Console.WriteLine($"Strength    : {strength}");
+            Console.WriteLine($"Blood Echoes: {bloodEchoes}");
+            Console.WriteLine($"Bloodtinge  : {bloodtinge}");
+            Console.WriteLine($"Insight     : {insight}");
+            Console.WriteLine($"Hunter Rank : {hunterRank}");
+            Console.WriteLine($"InNightmare  : {isInNightmare}");
+            Console.WriteLine();
+
+            Console.WriteLine("=================================");
+        }
+    }
+}
