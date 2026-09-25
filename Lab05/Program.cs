864303572057d5264bef2c3436ca1eb37f88159d
KUNTAPHAT <peetlock@gmail.com>
2026-09-25 20:30:50 +0700

    Update Program.cs

--- Lab05/Program.cs
+++ Lab05/Program.cs
@@ -1,4 +1,11 @@
-﻿namespace Lab05
+﻿/*
+* Student ID : 1690701451
+* Name       : Lab02
+* Section    : 129B
+* No.        : N/A
+* Course     : GI113 Computer Programming (GI)
+*/
+namespace Lab05
 {
     internal class Program
     {
